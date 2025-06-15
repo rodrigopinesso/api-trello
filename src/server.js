68597e8442657b12f8184bb49e40e13ea3c50cb1
Rodrigo Pinesso <rodrigopinesso123@gmail.com>
2025-06-14 23:44:46 -0300

    feat: configura servidor Express e conexão MongoDB

---
+++ src/server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const app = express();
+app.use(express.json());
+
+// Rota teste
+app.get('/', (req, res) => {
+  res.send('API Task rodando...');
+});
+
+const PORT = process.env.PORT || 3000;
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log('mongo conectou');
+    app.listen(PORT, () => console.log(`servidor rodando na ${PORT}`));
+  })
+  .catch((err) => console.error('mongo nao conectou:', err));
